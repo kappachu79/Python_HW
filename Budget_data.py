9f9883be6431b4b6007f2b006bcca1552ba187d1
kappachu79 <65929853+kappachu79@users.noreply.github.com>
2020-06-23 18:06:26 -0400

    Add files via upload

--- Budget_data.py
+++ Budget_data.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-input_budget = os.path.join("..", "Resources", "budget_data.csv")
+input_budget = os.path.join("budget_data.csv")
 
 # List to store budget_data
 months_total = []
@@ -13,13 +13,13 @@ with open(input_budget) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
 # Skip header
-header = next(csvreader)
+    header = next(csvreader)
 
 # Loop through looking for rows is the csv file
-for row in csvreader:
+    for row in csvreader:
 
-    months_total.append(row[0])
-    profit_total.append(int(row[1]))
+        months_total.append(row[0])
+        profit_total.append(int(row[1]))
 
 # loop over profit/loss column to get change in profit s
 for i in range(len(profit_total)-1):
@@ -46,3 +46,15 @@ print(f"Average Change: {round(sum(profit_change)/len(profit_change),2)}")
 print(f"Greatest Increase in Profits: {months_total[max_increase_month]} (${(str(max_increase_value))})")
 print(f"Greatest Decrease in Profits: {months_total[max_decrease_month]} (${(str(max_decrease_value))})")
 
+ # Set variable for output file
+output_file = os.path.join("budget_final.csv")
+
+#  Open the output file
+with open(output_file, "w") as datafile:
+    writer = csv.writer(datafile)
+
+    # Write the header row
+    writer.writerow(["Total Months", "profit_total", "Average Change", "Greatest Increase in Profits",
+                     "Greatest Decrease in Profits"])
+
+    
